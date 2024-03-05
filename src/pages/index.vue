8d52e9bdc9e2b7e85d5ac346de29b3d7aa8fb8db
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-05 15:04:14 +0100

    Liste d'exemples

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,28 +1,63 @@
 <script setup lang="ts">
 import BasketProfil from "../components/BasketProfil.vue";
 import type { Basket } from "../types";
-const exemple0: Basket = {
-  semelle: "#FFFF00",
-  empeigne: "#FFFFFF",
-  pointe: "#FFFFFF",
-  oeillet: "#FFFFFF",
-  bande: "#00FF00",
-  languette: "#FFFFFF",
-  lacet: "#00FF00",
-  trimestre: "#FFFFFF",
-};
+
+// const exemple0: Basket = {
+//   semelle: "#FFFF00",
+//   empeigne: "#FFFFFF",
+//   pointe: "#FFFFFF",
+//   oeillet: "#FFFFFF",
+//   bande: "#00FF00",
+//   languette: "#FFFFFF",
+//   lacet: "#00FF00",
+//   trimestre: "#FFFFFF",
+// };
+const exemples: Basket[] = [
+  {
+    semelle: "#FFFF00",
+    empeigne: "#FFFFFF",
+    pointe: "#FFFFFF",
+    oeillet: "#FFFFFF",
+    bande: "#00FF00",
+    languette: "#FFFFFF",
+    lacet: "#00FF00",
+    trimestre: "#FFFFFF",
+  },
+  {
+    semelle: "#FFFFFF",
+    empeigne: "#FFFF00",
+    pointe: "#FFFFFF",
+    oeillet: "#FFFFFF",
+    bande: "#00FF00",
+    languette: "#FFFFFF",
+    lacet: "#00FFFF",
+    trimestre: "#FFFFFF",
+  },
+  {
+    semelle: "#FFFFFF",
+    empeigne: "#FF0000",
+    pointe: "#00FFFF",
+    oeillet: "#FFFFFF",
+    bande: "#00FF00",
+    languette: "#FFFFFF",
+    lacet: "#00FFFF",
+    trimestre: "#FFFFFF",
+  }
+];
 </script>
+
+
+
+
 <template>
   <section>
     <h1 class="text-2xl">Exemples de Baskets</h1>
     <div class="flex flex-wrap gap-2">
-      <div class="w-64">
-        <RouterLink
-          :to="{
-            name: '/basket/exemple/[data]',
-            params: { data: JSON.stringify(exemple0) },
-          }"
-        >
+      <div class="w-64" v-for="exemple0 in exemples">
+        <RouterLink :to="{
+        name: '/basket/exemple/[data]',
+        params: { data: JSON.stringify(exemple0) },
+      }">
           <BasketProfil class="w-64" v-bind="exemple0" />
         </RouterLink>
       </div>
