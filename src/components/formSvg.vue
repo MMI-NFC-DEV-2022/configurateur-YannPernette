72759ed437b079c14cf09ce71aa809f42178f5bd
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-05 11:45:40 +0100

    form couleur semelle profil fonctionnel

---
+++ src/components/formSvg.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { ChaussureSvg } from "@/types-svg";
+import { ref } from "vue";
+import SvgProfil from "./svgProfil.vue";
+import SvgDessus from "./icons/vueDessus.vue";
+
+const chaussure = ref<ChaussureSvg>({});
+</script>
+
+
+
+<template>
+    <div class="p-2">
+        <ul class="flex gap-1">
+            <li><a href="#profil">Profil</a></li>
+            <li><a href="#dessus">Dessus</a></li>
+        </ul>
+        <div class="carousel w-64">
+            <SvgProfil class="carousel-item w-64" v-bind="chaussure" id="profil" />
+            <SvgDessus class="carousel-item w-64" v-bind="chaussure" id="dessus" />
+        </div>
+        <FormKit type="form" v-model="chaussure">
+            <FormKit name="semelle" label="semelle" value="#FFFFFF" type="color" />
+            <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="color" />
+        </FormKit>
+    </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import svgProfil from '@/components/svgProfil.vue';
+import formSvg from '@/components/formSvg.vue';
+</script>
 
 <template>
-  <main>Agence Immo</main>
+  <main>
+
+    <div>
+      <formSvg />
+    </div>
+
+  </main>
 </template>
