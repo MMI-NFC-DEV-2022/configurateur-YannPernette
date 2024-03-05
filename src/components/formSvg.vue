7d0d691ca80003a46697e56d12d73f2d860577f8
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2024-03-05 13:37:44 +0100

    changements de couleur fonctionnels (profil et dessus)

--- src/components/formSvg.vue
+++ src/components/formSvg.vue
@@ -22,12 +22,12 @@ const chaussure = ref<ChaussureSvg>({});
         <FormKit type="form" v-model="chaussure">
             <FormKit name="semelle" label="semelle" value="#FFFFFF" type="color" />
             <FormKit name="empeigne" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="pointe" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="oeillet" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="bande" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="languette" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="lacet" label="empeigne" value="#FFFFFF" type="color" />
-            <FormKit name="trimestre" label="empeigne" value="#FFFFFF" type="color" />
+            <FormKit name="pointe" label="pointe" value="#FFFFFF" type="color" />
+            <FormKit name="oeillet" label="oeillet" value="#FFFFFF" type="color" />
+            <FormKit name="bande" label="bande" value="#FFFFFF" type="color" />
+            <FormKit name="languette" label="languette" value="#FFFFFF" type="color" />
+            <FormKit name="lacet" label="lacet" value="#FFFFFF" type="color" />
+            <FormKit name="trimestre" label="trimestre" value="#FFFFFF" type="color" />
         </FormKit>
     </div>
 </template>
